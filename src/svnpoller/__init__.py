60b0715eefa44ea7177ad394b2900f010f1af02a
shimizukawa <shimizukawa@localhost>
2010-05-20 02:32:16 +0900

    add sendmail

--- src/svnpoller/__init__.py
+++ src/svnpoller/__init__.py
@@ -1,26 +1,54 @@
 import os, sys, subprocess
-from ConfigParser import SafeConfigParser
+from ConfigParser import ConfigParser
 from StringIO import StringIO
 from lxml import etree
+import sendmail
 
 CONFIG_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'svnpoller.ini')
-DEFAULTS = dict(url=None, address=None, newest_rev=None)
 
 ENV = os.environ.copy()
 ENV['LANG'] = 'C'
 
 POPEN_KW = dict(stdin=subprocess.PIPE, stdout=subprocess.PIPE, env=ENV)
 
+MAIL_TEMPLATE = """
+ Revision: %(rev)s
+ Author: %(auth)s
+ Date: %(date)s
+ Message:
+%(msg)s
 
-def run(*args, **kw):
-    conf = SafeConfigParser(DEFAULTS)
-    conf.read(CONFIG_PATH)
+%(diff)s
+"""
+
+def build_message(rev, auth, date, msg, diff):
+    return MAIL_TEMPLATE % locals()
+
+
+def run():
+    if len(sys.argv) > 1:
+        config_file = sys.argv[1]
+    else:
+        config_file = CONFIG_PATH
+    conf = ConfigParser()
+    conf.read(config_file)
+
+    mail_data = dict(
+        smtpserver = conf.get('mail','smtpserver'),
+        fromaddr = conf.get('mail','fromaddr'),
+    )
+
     for sect in conf.sections():
-        svn_log = ['svn', 'log', '--xml']
-        newest_rev = conf.get(sect, 'newest_rev')
+        if sect == 'mail':
+            continue
+
+        opts = dict(conf.items(sect))
+
+        svn_log = ['svn', 'log', '-v', '--xml']
+        newest_rev = opts.get('newest_rev', None)
         if newest_rev:
             svn_log.append('-r%d:HEAD' % (int(newest_rev)+1))
-        svn_log.append(conf.get(sect, 'url'))
+        svn_log.append(opts.get('url'))
         proc = subprocess.Popen(svn_log, **POPEN_KW)
         xml_data = proc.stdout.read()
 
@@ -29,17 +57,26 @@ def run(*args, **kw):
             rev = node.attrib['revision']
             svn_diff = ['svn', 'diff']
             svn_diff.append('-c%s' % rev)
-            svn_diff.append(conf.get(sect, 'url'))
+            svn_diff.append(opts.get('url'))
             proc = subprocess.Popen(svn_diff, **POPEN_KW)
             diff_data = proc.stdout.read()
-            print '---------------'
-            print ' Revision: %s' % rev
-            print ' Author: %s' % node.find('author').text
-            print ' Date: %s' % node.find('date').text
-            print ' Message:'
-            print node.find('msg').text
-            print
-            print diff_data
+
+            text = build_message(
+                    rev,
+                    node.find('author').text,
+                    node.find('date').text,
+                    node.find('msg').text,
+                    diff_data)
+
+            subject = '[%(sect)s: %(rev)s]' % locals()
+
+            sendmail.send(
+                    mail_data['fromaddr'],
+                    opts.get('address'),
+                    subject,
+                    text,
+                    smtpserver=mail_data['smtpserver'])
+
             conf.set(sect, 'newest_rev', rev)
 
     conf.write(open(CONFIG_PATH, 'wt'))
--- src/svnpoller/sendmail.py
+++ src/svnpoller/sendmail.py
@@ -1,7 +1,12 @@
 import smtplib
 from types import ListType, TupleType, StringType
+from email.MIMEText import MIMEText
+from email.Header import Header
 
-def sendmail(fromaddr, toaddrs, msg, smtpserver):
+
+def send(fromaddr, toaddrs, subject, message_text,
+         smtpserver='localhost', charset='utf-8'):
+
     if type(toaddrs) in (ListType, TupleType):
         pass
     elif type(toaddrs) in (StringType,):
@@ -9,13 +14,56 @@ def sendmail(fromaddr, toaddrs, msg, smtpserver):
     else:
         raise TypeError, toaddrs
 
-    # Add the From: and To: headers at the start!
-    #sendmsg = "From: %s\r\nTo: %s\r\n\r\n%s" % (fromaddr, ", ".join(toaddrs), msg)
-    sendmsg = msg
+    default_charset = guess_charset(message_text)
+    text = u'\n'.join(safe_decode(x, default_charset)
+                      for x in message_text.splitlines())
+    msg = buildmail(charset, fromaddr, toaddrs, subject, text)
+    return _sendmail(fromaddr, toaddrs, msg.as_string(), smtpserver)
 
+
+def guess_charset(data):
+    f = lambda d, enc: d.decode(enc) and enc
+
+    try: return f(data, 'utf-8')
+    except: pass
+    try: return f(data, 'cp932')
+    except: pass
+    try: return f(data, 'shift-jis')
+    except: pass
+    try: return f(data, 'euc-jp')
+    except: pass
+    try: return f(data, 'iso2022-jp')
+    except: pass
+    return None
+
+
+def safe_decode(data, default=None):
+    charset = guess_charset(data) or default
+    if charset:
+        return data.decode(charset,'replace')
+    return data
+
+
+def buildmail(charset, fromaddr, toaddrs, subject, message):
+    m_body = message.encode(charset, 'replace')
+    m_subject = subject
+    m_subject = Header(m_subject.encode(charset, 'replace'), charset)
+    m_from = fromaddr
+    m_to = ', '.join(toaddrs)
+    message = MIMEText(m_body, 'plain', charset)
+    message['Subject'] = m_subject
+    message['From'] = m_from
+    message['To'] = m_to
+    return message
+
+
+def _sendmail(fromaddr, toaddrs, msg, smtpserver):
     server = smtplib.SMTP(smtpserver)
     #server.set_debuglevel(1)
-    server.sendmail(fromaddr, toaddrs, sendmsg)
+    server.sendmail(fromaddr, toaddrs, msg)
     server.quit()
 
+
+def _sendmail(fromaddr, toaddrs, msg, smtpserver):
+    print msg
 
